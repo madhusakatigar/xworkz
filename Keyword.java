e97e82d09e4430ebc0a4130197cff352eafb63fb
madhusakatigar <126573310+madhusakatigar@users.noreply.github.com>
2023-03-07 19:27:06 +0530

    Add files via upload

---
+++ Keyword.java
@@ -0,0 +1,25 @@
+public class Keyword {
+    public static void main(String[] args) {
+        String[] animal = new String[5];
+        String[] car = new String[6];
+        String[] bike = new String[6];
+        String[] sports = new String[6];
+        String[] cities = new String[6];
+        String[] state = new String[6];
+        String[] phoneBrand = new String[8];
+        String[] birds = new String[7];
+        String[] laptop = new String[9];
+        String[] games = new String[5];
+        System.out.println(animal[2]);
+        System.out.println(car[2]);
+        System.out.println(bike[3]);
+        System.out.println(sports[2]);
+        System.out.println(cities[2]);
+        System.out.println(state[2]);
+        System.out.println(phoneBrand[2]);
+        System.out.println(birds[2]);
+        System.out.println(laptop[2]);
+        System.out.println(games[2]);
+    }
+    
+}
