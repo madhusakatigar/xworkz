16291f5f35ac85d486cae6875a422970b8441c88
madhusakatigar <126573310+madhusakatigar@users.noreply.github.com>
2023-03-07 16:11:21 +0530

    Add files via upload

---
+++ Literal.java
@@ -0,0 +1,26 @@
+public class Literal {
+    public static void main(String[] args) {
+        String car = "BMW";
+        String bike = "Ninja";
+        String animals = "Dog";
+        String sports = "Cricket";
+        String cities = "Gadag";
+        String state = "Karnataka";
+        String phoneBrand = "Redmi";
+        String birds = "Peacock";
+        String laptop = "Asus";
+        String games = "BGMI";
+        System.out.println(car);
+        System.out.println(bike);
+        System.out.println(animals);
+        System.out.println(sports);
+        System.out.println(cities);
+        System.out.println(state);
+        System.out.println(phoneBrand);
+        System.out.println(birds);
+        System.out.println(laptop);
+        System.out.println(games);
+
+    }
+    
+}
