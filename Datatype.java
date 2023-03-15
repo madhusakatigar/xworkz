2c68287c2d0829c7db4e6d5a1cfa32bd0a8ac5f0
madhusakatigar <126573310+madhusakatigar@users.noreply.github.com>
2023-03-15 13:32:01 +0530

    Add files via upload

---
+++ Datatype.java
@@ -0,0 +1,24 @@
+public class Datatype {
+    public static void main(String[] args) {
+int[] num = {1,2,3,4,5,6,7,8,9,10};
+double[] num1 = {1.1,2.2,3.3,4.4,5.5,6.6,7.7,8.8,9.9,10.10};
+char[] name = {'A','B','C','D','E','F','G','H','I','J'};
+byte[] mybyte = {10,20,30,40,50,60,70,80,90,100};
+short[] myshort ={11,15,9,32767,-32768,18,45,78,98,-158}; 
+long[] longnum = {123456789L,234567891L,345678912L,456789123L,567894123L,678945123L,789654321L,897654321L,987654321L,147258369L};
+float[] myfloat = {0.1F,0.2F,3.12F,0.4F,0.5F,1.3F,1.5F,1.6F,1.8F,1.9F};
+boolean[] myboolean = {true,false,true,false,true,false,true,false,true,false};
+String[] str = {"Madhu","Sandeep","Vinayak","Girish","Nikhita","Manjunath","Rocky","Ganesh","Ambika","Priyanka"};
+int[] myarry = {2,4,5,6,8,10,12,14,16,18,20};
+System.out.println(num);
+System.out.println(num1);
+System.out.println(name);
+System.out.println(mybyte);
+System.out.println(myshort);
+System.out.println(longnum);
+System.out.println(myfloat);
+System.out.println(myboolean);
+System.out.println(str);
+System.out.println(myarry);
+    }
+}
