57f623c11ab74bdcd6317e96f718d970673f7e14
madhusakatigar <126573310+madhusakatigar@users.noreply.github.com>
2023-03-15 21:39:19 +0530

    Add files via upload

---
+++ Object.java
@@ -0,0 +1,24 @@
+public class Object {
+    int a = 10;
+
+    public static void main(String[] args) {
+        Object o1 = new Object();
+        System.out.println(o1.a);
+        
+
+    }
+
+    int sum(){
+        int x = 10;
+        int y = 20;
+
+        int z = x + y;
+
+        return z;
+        
+    }
+
+   
+
+
+}
