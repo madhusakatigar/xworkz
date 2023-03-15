66c5047159525ddfac789ce61c3e9ecdeb523403
madhusakatigar <126573310+madhusakatigar@users.noreply.github.com>
2023-03-15 17:33:22 +0530

    Add files via upload

---
+++ Method.java
@@ -0,0 +1,20 @@
+public class Method{
+	public static void add(){
+		int A = 10;
+		int B = 20;
+		int C = A+B;
+		System.out.println(C);
+ 
+	}
+	public static void sub(){
+		int num1 = 20;
+		int num2 = 10;
+		int total = num1-num2;
+		System.out.println(total);
+	}
+	public static void main(String[] args) {
+		add();
+		sub();
+	}
+
+}
