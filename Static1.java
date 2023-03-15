692fd5bf59d43dab6559272bd3a2265d677b592f
madhusakatigar <126573310+madhusakatigar@users.noreply.github.com>
2023-03-15 21:38:30 +0530

    Add files via upload

---
+++ Static1.java
@@ -0,0 +1,18 @@
+public class Static1 extends Test{
+    public static void main(String[] args) {
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+
+
+        Test st = new Test();
+        System.out.println(st.A);
+        System.out.println(st.B);
+        System.out.println(st.C);
+        System.out.println(st.D);
+        System.out.println(st.E);
+    }
+}
