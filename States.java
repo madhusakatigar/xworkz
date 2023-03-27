564744cfb9e6c831f068bbb9cd9e5b99ccf5f836
madhusakatigar <126573310+madhusakatigar@users.noreply.github.com>
2023-03-27 22:38:16 +0530

    Add files via upload

---
+++ States.java
@@ -0,0 +1,176 @@
+class States{
+	static int num;
+	static String str;
+	static boolean isUpper;
+	static char c;
+	static int sum;
+	static int districts;
+	static string tourist;
+	static string metropolitianCities;
+	static int numberofaverageCities;
+	static boolean largesrState;
+	static long area;
+	static string stateFlagcolour;
+	static string stateAnimal;
+	static boolean smollestState;
+	static string oneName;
+	static int cars;
+	static int bikes;
+	static int bus;
+	static int totalComapnys;
+
+
+
+	final String xyz;
+	final int fib;
+	final boolean isLower;
+	final String urn;
+	final char s;
+	final long area;
+	final boolean bigestState;
+	final short carnum;
+	final long statenum;
+	final short dog;
+	final boolean largestCitiesthere;
+	final string smollestVillagename;
+
+
+	byte hunk;
+	long num1;
+	short num2;
+	int oil;
+	String title;
+	float f;
+	double test;
+	int fab;
+	short text;
+	boolean isLegal;
+	byte books;
+	long num4;
+	short num3;
+	int bill;
+	String dogsBreades;
+	float nick;
+	double rest;
+	int stg;
+	short bgh;
+	boolean jhg;
+
+
+
+	public void myStr(){
+
+	}
+
+	public void Defn(){
+		
+	}
+
+	public void Yest(){
+		
+	}
+
+	public void Live(){
+		
+	}
+
+	public void Respect(){
+		
+	}
+
+	public void Total(){
+		
+	}
+
+	public void Tsum(){
+		
+	}
+
+	public void Fast(){
+		
+	}
+
+	public void Kite(){
+		
+	}
+
+	public void abc(){
+		
+	}
+
+	public void dog(){
+		
+	}
+
+	public void country(){
+		
+	}
+	public void districts(){
+		
+	}
+	public void solo(){
+		
+	}
+	public void sit(){
+		
+	}
+	public void table(){
+		
+	}
+	public void cable(){
+		
+	}
+	public void stable(){
+		
+	}
+
+	public void tasvsg(){
+		
+	}
+	public void tasnshe(){
+		
+	}
+	public void tablsg(){
+		
+	}
+	public void tvgdvle(){
+		
+	}
+	public void tjgdsge(){
+		
+	}
+	public void hdle(){
+		
+	}
+	public void dshg(){
+		
+	}
+	public void dsdbmne(){
+		
+	}
+	public void hble(){
+		
+	}
+	public void sle(){
+		
+	}
+	public void tae(){
+		
+	}
+	public void tle(){
+		
+	}
+	public void able(){
+		
+	}
+	public void tab(){
+		
+	}
+	public void vsdv(){
+		
+	}
+	public void jsck(){
+		
+	}
+
+}
